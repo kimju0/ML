179f8e77f272d1261b769ff0f2d6189ce623b0be
kimju0 <kimuniverse0@naver.com>
2025-04-17 13:12:02 +0900

    gridSearch, svm

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas
 from torch.utils.data import DataLoader
 from torchvision import transforms, datasets
 import numpy as np
@@ -8,7 +9,51 @@ from scipy.stats import randint
 from sklearn.model_selection import GridSearchCV
 
 
-def main():
+def decision_tree(CIFAR_train_images, CIFAR_train_labels, CIFAR_test_images, CIFAR_test_labels,
+                  MNIST_train_images, MNIST_train_labels, MNIST_test_images, MNIST_test_labels):
+    # GridSearchCV
+    params_grid = {"max_depth": [3, 6, 9, 12],
+                   'min_samples_split': [2, 5, 10],
+                   'min_samples_leaf': [1, 2, 4],
+                   'max_leaf_nodes': [5, 10, None]}
+    grid_search = GridSearchCV(DecisionTreeClassifier(criterion='entropy'), params_grid, cv=5, n_jobs=-1)
+    grid_search.fit(CIFAR_train_images, CIFAR_train_labels)
+    print("CIFAR-10 Best parameters:", grid_search.best_params_)
+    print("CIFAR-10 Best cross-validation score:", grid_search.best_score_)
+    results = pandas.DataFrame(grid_search.cv_results_)
+    print(results[['params', 'mean_test_score', 'std_test_score']])
+
+    grid_search.fit(MNIST_train_images, MNIST_train_labels)
+    print("MNIST Best parameters:", grid_search.best_params_)
+    print("MNIST Best cross-validation score:", grid_search.best_score_)
+    results = pandas.DataFrame(grid_search.cv_results_)
+    print(results[['params', 'mean_test_score', 'std_test_score']])
+
+
+def SVM(CIFAR_train_images, CIFAR_train_labels, CIFAR_test_images, CIFAR_test_labels,
+        MNIST_train_images, MNIST_train_labels, MNIST_test_images, MNIST_test_labels):
+    # SVM
+    params_grid = {
+        'C': [0.1, 1, 10],
+        'gamma': ['scale', 'auto'],
+        'kernel': ['linear', 'rbf']
+    }
+    grid_search = GridSearchCV(svm.SVC(), params_grid, cv=5, n_jobs=-1)
+    grid_search.fit(CIFAR_train_images, CIFAR_train_labels)
+    print("CIFAR-10 Best parameters:", grid_search.best_params_)
+    print("CIFAR-10 Best cross-validation score:", grid_search.best_score_)
+    results = pandas.DataFrame(grid_search.cv_results_)
+    print(results[['params', 'mean_test_score', 'std_test_score']])
+
+    grid_search.fit(MNIST_train_images, MNIST_train_labels)
+    print("MNIST Best parameters:", grid_search.best_params_)
+    print("MNIST Best cross-validation score:", grid_search.best_score_)
+    results = pandas.DataFrame(grid_search.cv_results_)
+    print(results[['params', 'mean_test_score', 'std_test_score']])
+
+
+
+def load_data():
     # CIFAR-10
     CIFAR_transform_train = transforms.Compose([transforms.ToTensor()])
     CIFAR_transform_test = transforms.Compose([transforms.ToTensor()])
@@ -66,29 +111,19 @@ def main():
         MNIST_test_labels.append(labels.numpy())
     MNIST_test_images = np.vstack(MNIST_test_images)
     MNIST_test_labels = np.concatenate(MNIST_test_labels)
+    return (CIFAR_train_images, CIFAR_train_labels, CIFAR_test_images, CIFAR_test_labels,
+            MNIST_train_images, MNIST_train_labels, MNIST_test_images, MNIST_test_labels)
 
-    from sklearn.tree import DecisionTreeClassifier
-    for i in (3, 6, 9, 12):
-        print("max_depth: {}".format(i))
 
-        tree = DecisionTreeClassifier(criterion='entropy',
-                                      max_depth=i)
-        tree.fit(CIFAR_train_images, CIFAR_train_labels)
+def main():
+    CIFAR_train_images, CIFAR_train_labels, CIFAR_test_images, CIFAR_test_labels, \
+        MNIST_train_images, MNIST_train_labels, MNIST_test_images, MNIST_test_labels = load_data()
 
-        print("CIFAR-10 훈련 세트 정확도: {:.3f}".format(tree.score(CIFAR_train_images, CIFAR_train_labels)))
-        print("CIFAR-10 테스트 세트 정확도: {:.3f}".format(tree.score(CIFAR_test_images, CIFAR_test_labels)))
+    decision_tree(CIFAR_train_images, CIFAR_train_labels, CIFAR_test_images, CIFAR_test_labels,
+                  MNIST_train_images, MNIST_train_labels, MNIST_test_images, MNIST_test_labels)
 
-        tree = DecisionTreeClassifier(criterion='entropy',
-                                      max_depth=i)
-        tree.fit(MNIST_train_images, MNIST_train_labels)
-        print("MNIST 훈련 세트 정확도: {:.3f}".format(tree.score(MNIST_train_images, MNIST_train_labels)))
-        print("MNIST 테스트 세트 정확도: {:.3f}".format(tree.score(MNIST_test_images, MNIST_test_labels)))
-
-    params_grid = {'min_samples_split': [2, 5, 10],
-                   'min_samples_leaf': [1, 2, 4],
-                   'max_leaf_nodes': [5, 10, None]}
-    grid_search = GridSearchCV(DecisionTreeClassifier(criterion='entropy', max_depth=3), params_grid, cv=5, n_jobs=-1)
-    print(grid_search.fit(MNIST_train_images, MNIST_train_labels))
+    SVM(CIFAR_train_images, CIFAR_train_labels, CIFAR_test_images, CIFAR_test_labels,
+        MNIST_train_images, MNIST_train_labels, MNIST_test_images, MNIST_test_labels)
 
 
 if __name__ == '__main__':
